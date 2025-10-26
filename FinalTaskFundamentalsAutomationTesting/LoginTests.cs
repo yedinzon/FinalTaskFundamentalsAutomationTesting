214923202fa8d4338d1045277ed99e9be1c14596
Yedinzon Toro Gil <yedinzon.toro@praxedes.co>
2025-10-26 09:23:14 -0500

    chore: initialize project with dependencies and basic test

---
+++ FinalTaskFundamentalsAutomationTesting/LoginTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FinalTaskFundamentalsAutomationTesting;
+
+[TestClass]
+public class LoginTests
+{
+    private readonly IWebDriver _webDriver;
+    public LoginTests()
+    {
+        var options = new ChromeOptions();
+        _webDriver = new ChromeDriver(options);
+    }
+
+    [TestMethod]
+    public void Test()
+    {
+        _webDriver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+        try
+        {
+            Assert.AreEqual("Swag Labs", _webDriver.Title);
+        }
+        finally
+        {
+            _webDriver.Quit();
+        }
+    }
+}
