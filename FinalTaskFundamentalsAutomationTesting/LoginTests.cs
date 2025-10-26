e6e4720b40fba6d885ba5a77476ea2fd75076a2c
Yedinzon Toro Gil <yedinzon.toro@praxedes.co>
2025-10-26 10:18:54 -0500

    test: add initial unit test

--- FinalTaskFundamentalsAutomationTesting/LoginTests.cs
+++ FinalTaskFundamentalsAutomationTesting/LoginTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 
 namespace FinalTaskFundamentalsAutomationTesting;
 
@@ -15,13 +17,40 @@ public class LoginTests
     }
 
     [TestMethod]
-    public void Test()
+    public void LoginWithEmptyCredentials_ShouldShow_UsernameError()
     {
         _webDriver.Navigate().GoToUrl("https://www.saucedemo.com/");
 
+        var usernameInput = _webDriver.FindElement(By.Id("user-name"));
+        var passwordInput = _webDriver.FindElement(By.Id("password"));
+        var loginButton = _webDriver.FindElement(By.Id("login-button"));
+
+        usernameInput.SendKeys("user-test1");
+        passwordInput.SendKeys("password-test");
+
+        var cleanInputs = new Actions(_webDriver);
+
+        cleanInputs
+            .Click(usernameInput)
+            .KeyDown(Keys.Control).SendKeys("a").KeyUp(Keys.Control)
+            .SendKeys(Keys.Delete)
+            .Click(passwordInput)
+            .KeyDown(Keys.Control).SendKeys("a").KeyUp(Keys.Control)
+            .SendKeys(Keys.Delete)
+            .Perform();
+
+        new WebDriverWait(_webDriver, TimeSpan.FromSeconds(2))
+            .Until(driver =>
+                usernameInput.GetAttribute("value") == "" &&
+                passwordInput.GetAttribute("value") == "");
+
+        loginButton.Click();
+
+        var errorMessage = _webDriver.FindElement(By.CssSelector("h3[data-test='error']"));
+
         try
         {
-            Assert.AreEqual("Swag Labs", _webDriver.Title);
+            Assert.IsTrue(errorMessage.Text.Contains("Username is required"));
         }
         finally
         {
